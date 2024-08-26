6020302fe42a028c6926c2f5b77f114b9776bfbc
Adolfo Cedric <adocssarillo@addu.edu.ph>
2024-08-26 15:20:45 +0800

    added the layout component. not yet finished

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import { useEffect, useState, FormEvent } from 'react';
+import Layout from '@/components/Layout';
 
 interface Employee {
     id: number;
@@ -206,99 +207,103 @@ export default function EmployeesPage() {
     const inactiveEmployees = employees.filter(employee => employee.is_archive === 1 || employee.is_archive === true);
 
     return (
-        <div className='container m-4'>
-            <h1 className='text-2xl mb-4 font-bold'>Employees</h1>
-            <div className='input-form'>
-                <form onSubmit={editingEmployee ? saveEmployee : addEmployee}>
-                    <div>
-                        <label htmlFor='first_name'>First Name: </label>
-                        <input type='text' id='first_name' name='first_name' className='text-black' defaultValue={currentEmployee?.first_name || ''}/>
-                        <label htmlFor='last_name'>Last Name: </label>
-                        <input type='text' id='last_name' name='last_name' className='text-black' defaultValue={currentEmployee?.last_name || ''}/>
-                        <label htmlFor='email_address'>E-mail: </label>
-                        <input type='email' id='email_address' name='email_address' className='text-black'defaultValue={currentEmployee?.email_address || ''}/>
-                        <label htmlFor='contact_number'>Contact Number: </label>
-                        <input type='text' id='contact_number' name='contact_number' className='text-black' defaultValue={currentEmployee?.contact_number || ''}/>
-                    </div>
-                    <div className='mt-4'>
-                        <label htmlFor='role_id'>Role: </label>
-                        <select 
-                            id='role_id' 
-                            name='role_id' 
-                            className='text-black' 
-                            value={selectedRole} 
-                            onChange={(e) => setSelectedRole(e.target.value)}
-                        >
-                            <option value='' disabled>Select Role</option>
-                            {roles.map((role) => (
-                                <option key={role.id} value={role.id}>{role.name}</option>
-                            ))}
-                        </select>
-                        <label htmlFor='employee_type'>Employee Type: </label>
-                        <select 
-                            id='employee_type' 
-                            name='employee_type' 
-                            className='text-black' 
-                            value={selectedEmployeeType} 
-                            onChange={(e) => setSelectedEmployeeType(e.target.value)}
-                        >
-                            <option value='' disabled>Select Type</option>
-                            <option value='Full-Time'>Full-Time</option>
-                            <option value='Part-Time'>Part-Time</option>
-                        </select>
-
-                        <label htmlFor='username'>Username: </label>
-                        <input type='text' id='username' name='username' className='text-black' defaultValue={currentEmployee?.username || ''}/>
-                        <label htmlFor='password'>Password: </label>
-                        <input type='password' id='password' name='password' className='text-black' defaultValue={currentEmployee?.password || ''}/>
-                            
-                        <button type='submit' className='px-2 mx-2 border border-white'>{editingEmployee ? 'Save Employee' : 'Add Employee'}</button>
-                    </div>
-                </form>
-            </div>
-            <div className='employee-toggle-view'>
-                <button className='px-2 mx-1 mt-2 border border-white' onClick={() => setListView('active')}>Active</button>
-                <button className='px-2 mx-1 mt-2 border border-white' onClick={() => setListView('inactive')}>Inactive</button>
-            </div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th className="px-4 py-2 text-center">Name</th>
-                            <th className="px-4 py-2 text-center">E-mail</th>
-                            <th className="px-4 py-2 text-center">Contact Number</th>
-                            <th className="px-4 py-2 text-center">Role</th>
-                            <th className="px-4 py-2 text-center">Employee Type</th>
-                            <th className="px-4 py-2 text-center">Username</th>
-                            <th className="px-4 py-2 text-center">Password</th>
-                            <th className="px-4 py-2 text-center">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(listView === 'active' ? activeEmployees : inactiveEmployees).map((employee) => (
-                            <tr key={employee.id} >
-                                <td className='px-4'>{employee.last_name + ', ' + employee.first_name}</td>
-                                <td className='px-4'>{employee.email_address}</td>
-                                <td className='px-4'>{employee.contact_number}</td>
-                                <td className='px-4'>{employee.role_name}</td>
-                                <td className='px-4'>{employee.employee_type}</td>
-                                <td className='px-4'>{employee.username}</td>
-                                <td className='px-4'>{employee.password}</td>
-                                <td>
-                                    {listView === 'active' ? (
-                                        <>
-                                            <button onClick={() => editEmployee(employee.id)} className='px-2'>Edit</button>
-                                            <button onClick={() => archiveEmployee(employee.id)} className='px-2'>Archive</button>
-                                        </>
-                                    ) : (
-                                        <button onClick={() => unarchiveEmployee(employee.id)}>Unarchive</button>
-                                    )}
-                                </td>
+        <Layout defaultTitle='Employees' 
+            rightSidebarContent={
+                <h1 className='m-auto'>In working...</h1>
+            }
+            >
+            <div className='container m-4'>
+                <div className='input-form'>
+                    <form onSubmit={editingEmployee ? saveEmployee : addEmployee}>
+                        <div>
+                            <label htmlFor='first_name'>First Name: </label>
+                            <input type='text' id='first_name' name='first_name' className='text-black' defaultValue={currentEmployee?.first_name || ''}/>
+                            <label htmlFor='last_name'>Last Name: </label>
+                            <input type='text' id='last_name' name='last_name' className='text-black' defaultValue={currentEmployee?.last_name || ''}/>
+                            <label htmlFor='email_address'>E-mail: </label>
+                            <input type='email' id='email_address' name='email_address' className='text-black'defaultValue={currentEmployee?.email_address || ''}/>
+                            <label htmlFor='contact_number'>Contact Number: </label>
+                            <input type='text' id='contact_number' name='contact_number' className='text-black' defaultValue={currentEmployee?.contact_number || ''}/>
+                        </div>
+                        <div className='mt-4'>
+                            <label htmlFor='role_id'>Role: </label>
+                            <select
+                                id='role_id'
+                                name='role_id'
+                                className='text-black'
+                                value={selectedRole}
+                                onChange={(e) => setSelectedRole(e.target.value)}
+                            >
+                                <option value='' disabled>Select Role</option>
+                                {roles.map((role) => (
+                                    <option key={role.id} value={role.id}>{role.name}</option>
+                                ))}
+                            </select>
+                            <label htmlFor='employee_type'>Employee Type: </label>
+                            <select
+                                id='employee_type'
+                                name='employee_type'
+                                className='text-black'
+                                value={selectedEmployeeType}
+                                onChange={(e) => setSelectedEmployeeType(e.target.value)}
+                            >
+                                <option value='' disabled>Select Type</option>
+                                <option value='Full-Time'>Full-Time</option>
+                                <option value='Part-Time'>Part-Time</option>
+                            </select>
+                            <label htmlFor='username'>Username: </label>
+                            <input type='text' id='username' name='username' className='text-black' defaultValue={currentEmployee?.username || ''}/>
+                            <label htmlFor='password'>Password: </label>
+                            <input type='password' id='password' name='password' className='text-black' defaultValue={currentEmployee?.password || ''}/>
+            
+                            <button type='submit' className='px-2 mx-2 border border-white'>{editingEmployee ? 'Save Employee' : 'Add Employee'}</button>
+                        </div>
+                    </form>
+                </div>
+                <div className='employee-toggle-view'>
+                    <button className='px-2 mx-1 mt-2 border border-white' onClick={() => setListView('active')}>Active</button>
+                    <button className='px-2 mx-1 mt-2 border border-white' onClick={() => setListView('inactive')}>Inactive</button>
+                </div>
+                <div className='overflow-x-auto'>
+                    <table className='min-w-full'>
+                        <thead>
+                            <tr>
+                                <th className="px-4 py-2 text-center">Name</th>
+                                <th className="px-4 py-2 text-center">E-mail</th>
+                                <th className="px-4 py-2 text-center">Contact Number</th>
+                                <th className="px-4 py-2 text-center">Role</th>
+                                <th className="px-4 py-2 text-center">Employee Type</th>
+                                <th className="px-4 py-2 text-center">Username</th>
+                                <th className="px-4 py-2 text-center">Password</th>
+                                <th className="px-4 py-2 text-center">Actions</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {(listView === 'active' ? activeEmployees : inactiveEmployees).map((employee) => (
+                                <tr key={employee.id} >
+                                    <td className='px-4'>{employee.last_name + ', ' + employee.first_name}</td>
+                                    <td className='px-4'>{employee.email_address}</td>
+                                    <td className='px-4'>{employee.contact_number}</td>
+                                    <td className='px-4'>{employee.role_name}</td>
+                                    <td className='px-4'>{employee.employee_type}</td>
+                                    <td className='px-4'>{employee.username}</td>
+                                    <td className='px-4'>{employee.password}</td>
+                                    <td>
+                                        {listView === 'active' ? (
+                                            <>
+                                                <button onClick={() => editEmployee(employee.id)} className='px-2'>Edit</button>
+                                                <button onClick={() => archiveEmployee(employee.id)} className='px-2'>Archive</button>
+                                            </>
+                                        ) : (
+                                            <button onClick={() => unarchiveEmployee(employee.id)}>Unarchive</button>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </Layout>
     );
 };
