36fe030be7c435408dc68f234204280a911551bd
Pretty Faith <pfimisuarez@addu.edu.ph>
2024-09-18 01:34:00 +0800

    modified styling

--- src/components/forms/Orders.tsx
+++ src/components/forms/Orders.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import { ArrowDropUp, ArrowDropDown} from '@mui/icons-material';
 import styles from '../styles/Form.module.css';
 
 const OrderForm = ({ selectedProducts, setSelectedProducts, selectedBranch }) => {
@@ -144,7 +145,7 @@ const OrderForm = ({ selectedProducts, setSelectedProducts, selectedBranch }) =>
             <div className={styles.orderDetails}>
                 <div className={styles.headerContainer}>
                     <div className={styles.headerLeft}>
-                        <div className={styles.heading2}>Divine Jewel</div>
+                        <div className={styles.heading2}>Current Order</div>
                         {/* <div className={styles.primary}>{selectedBranch?.branch_name || 'No Branch'}</div>
                         <div className={styles.secondary}>{selectedBranch?.branch_address || 'No Address'}</div> */}
                     </div>
@@ -161,10 +162,10 @@ const OrderForm = ({ selectedProducts, setSelectedProducts, selectedBranch }) =>
 
                 {/* Customer Name and Current Order */}
                 <div className={styles.orderCustomerContainer}>
-                    <div className={styles.orderLeft}>
+                    {/* <div className={styles.orderLeft}>
                         <div className={styles.heading2}>Current Order</div>
-                    </div>
-                    <div className={styles.verticalLineShort}></div>
+                    </div> */}
+                    {/* <div className={styles.verticalLineShort}></div> */}
                     <div className={styles.customerRight}>
                         <input
                             type="text"
@@ -189,25 +190,22 @@ const OrderForm = ({ selectedProducts, setSelectedProducts, selectedBranch }) =>
                             <div className={styles.productName}>{product.name}</div>
                             <div className={styles.productQuantityWrapper}>
                                 <div className={styles.productQuantity}>
-                                    <button
-                                        className={styles.quantityButton}
+                                    <ArrowDropDown
+                                        // className={styles.quantityButton}
                                         onClick={() => handleQuantityChange(product.product_id, -1)}
                                         disabled={productQuantities[product.product_id] === 0}
-                                    >
-                                        -
-                                    </button>
+                                    />
+                            
                                     <input
                                         type="text"
                                         className={styles.customInput}
                                         value={productQuantities[product.product_id]}
                                         readOnly
                                     />
-                                    <button
-                                        className={styles.quantityButton}
+                                    <ArrowDropUp
+                                        // className={styles.quantityButton}
                                         onClick={() => handleQuantityChange(product.product_id, 1)}
-                                    >
-                                        +
-                                    </button>
+                                    />
                                 </div>
                             </div>
                             <div className={styles.productPrice}>{parseFloat(product.price).toFixed(2)}</div>
