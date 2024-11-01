11f592bb4948b11fef0702a0ff92f981a5918ee4
Adolfo Cedric <adocssarillo@addu.edu.ph>
2024-11-01 14:00:48 +0800

    transferred Sales Bar Chart from page.tsx  to TotalSalesChart.tsx component

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -40,7 +40,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-
 import {
   ChartConfig,
   ChartContainer,
@@ -54,6 +53,7 @@ import {
 } from "@/components/ui/popover"
 import { Sales, columns } from './columns';
 import { DataTable } from './data-table';
+import TotalSalesChart from './_components/TotalSalesChart';
 
 const timePeriod = [
   {
@@ -284,11 +284,8 @@ const pieChartConfig = {
 } satisfies ChartConfig
 
 const barChartConfig = {
-  views: {
-    label: "Page Views",
-  },
-  desktop: {
-    label: "Transactions",
+  sales: {
+    label: "Sales",
     color: "#FCB6D7",
   },
 } satisfies ChartConfig
@@ -306,14 +303,7 @@ export default function Home() {
   const [value, setValue] = useState("overall");
   const [selectedStore, setSelectedStore] = useState("all");
   // for Interactive Bar Chart
-  const [activeChart, setActiveChart] = React.useState<keyof typeof barChartConfig>("desktop");
-
-  const total = React.useMemo(
-    () => ({
-      desktop: chartData.reduce((acc, curr) => acc + curr.desktop, 0),
-    }),
-    []
-  )
+  const [activeChart, setActiveChart] = React.useState<keyof typeof barChartConfig>("sales");
 
   return (
     <MainLayout defaultTitle="Sales">
@@ -362,86 +352,11 @@ export default function Home() {
       </div>
       <div className="flex flex-col gap-2 mx-8 ">
         <div className="bg-gray-100 w-full rounded-2xl">
-          <Card>
-            <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row ">
-              <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
-                <CardTitle>Total Sales</CardTitle>
-                <CardDescription>
-                  Showing total sales for the last 3 months
-                </CardDescription>
-              </div>
-              <div className="flex">
-                {["desktop"].map((key) => {
-                  const chart = key as keyof typeof barChartConfig;
-                  return (
-                    <button
-                      key={chart}
-                      data-active={activeChart === chart}
-                      className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
-                      onClick={() => setActiveChart(chart)}
-                    >
-                      <span className="text-xs text-muted-foreground">
-                        {barChartConfig[chart].label}
-                      </span>
-                      <span className="text-lg font-bold leading-none sm:text-3xl">
-                        {total[key as keyof typeof total].toLocaleString()}
-                      </span>
-                    </button>
-                  );
-                })}
-              </div>
-            </CardHeader>
-            <CardContent className="px-2 sm:p-6">
-              <ChartContainer
-                config={barChartConfig}
-                className="aspect-auto h-[250px] w-full"
-              >
-                <BarChart
-                  accessibilityLayer
-                  data={chartData}
-                  margin={{
-                    left: 12,
-                    right: 12,
-                  }}
-                >
-                  <CartesianGrid vertical={false} />
-                  <XAxis
-                    dataKey="date"
-                    tickLine={false}
-                    axisLine={false}
-                    tickMargin={8}
-                    minTickGap={32}
-                    tickFormatter={(value) => {
-                      const date = new Date(value);
-                      return date.toLocaleDateString("en-US", {
-                        month: "short",
-                        day: "numeric",
-                      });
-                    }}
-                  />
-                  <ChartTooltip
-                    content={
-                      <ChartTooltipContent
-                        className="w-[150px]"
-                        nameKey="views"
-                        labelFormatter={(value) => {
-                          return new Date(value).toLocaleDateString("en-US", {
-                            month: "short",
-                            day: "numeric",
-                            year: "numeric",
-                          });
-                        }}
-                      />
-                    }
-                  />
-                  <Bar
-                    dataKey={activeChart}
-                    fill={`var(--color-${activeChart})`}
-                  />
-                </BarChart>
-              </ChartContainer>
-            </CardContent>
-          </Card>
+          <TotalSalesChart 
+            activeChart={activeChart} 
+            setActiveChart={setActiveChart} 
+            barChartConfig={barChartConfig}
+          />
         </div>
         <div className="bg-white h-[430px] shadow-md w-full rounded-2xl flex flex-col border border-gray-200">
           <div className="flex flex-row">
