82af5a020d7a1ab0e6801214edeb4481d9b63de1
Pretty Faith <pfimisuarez@addu.edu.ph>
2024-11-17 17:27:53 +0800

    .

--- src/app/dashboard/_components/TotalSalesChart.tsx
+++ src/app/dashboard/_components/TotalSalesChart.tsx
@@ -6,38 +6,57 @@ import {
     ChartContainer,
     ChartTooltip,
     ChartTooltipContent,
-  } from "@/components/ui/chart"
+} from "@/components/ui/chart"
 
-const chartConfig = {
-  views: {
-    label: "Page Views",
+const chartData = [
+  { date: "2023-07-01", sales: 2220 },
+  { date: "2023-08-01", sales: 1970 },
+  { date: "2023-09-01", sales: 1670 },
+  { date: "2023-10-01", sales: 2420 },
+  { date: "2023-11-01", sales: 3730 },
+  { date: "2023-12-01", sales: 3000 },
+  { date: "2024-01-01", sales: 2450 },
+  { date: "2024-02-01", sales: 4090 },
+  { date: "2024-03-01", sales: 590 },
+  { date: "2024-04-01", sales: 2610 },
+  { date: "2024-05-01", sales: 3270 },
+  { date: "2024-06-01", sales: 2920 },
+];
+
+const barChartConfig = {
+  sales: {
+    label: "Sales",
+    color: "#FCB6D7",
   },
-  desktop: {
-    label: "Transactions",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
+} satisfies ChartConfig
 
 interface TotalSalesChartProps {
-  activeChart: keyof typeof chartConfig;
-  setActiveChart: (chart: keyof typeof chartConfig) => void;
-  total: { [key: string]: number };
-  chartData: { date: string; [key: string]: number }[];
+  activeChart: keyof typeof barChartConfig;
+  setActiveChart: (chart: keyof typeof barChartConfig) => void;
+  barChartConfig: ChartConfig;
 }
+  
 
-const TotalSalesChart: React.FC<TotalSalesChartProps> = ({ activeChart, setActiveChart, total, chartData }) => {
+const TotalSalesChart: React.FC<TotalSalesChartProps> = ({ activeChart, setActiveChart, barChartConfig }) => {
+  const total = React.useMemo(
+    () => ({
+      sales: chartData.reduce((acc, curr) => acc + curr.sales, 0),
+    }),
+    []
+  )
+
   return (
     <Card>
-      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row ">
         <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
           <CardTitle>Total Sales</CardTitle>
           <CardDescription>
-            Showing total visitors for the last 3 months
+            Showing total sales for the last 12 months
           </CardDescription>
         </div>
-        <div className="flex">
-          {["desktop"].map((key) => {
-            const chart = key as keyof typeof chartConfig;
+        {/* <div className="flex">
+          {["sales"].map((key) => {
+            const chart = key as keyof typeof barChartConfig;
             return (
               <button
                 key={chart}
@@ -46,7 +65,7 @@ const TotalSalesChart: React.FC<TotalSalesChartProps> = ({ activeChart, setActiv
                 onClick={() => setActiveChart(chart)}
               >
                 <span className="text-xs text-muted-foreground">
-                  {chartConfig[chart].label}
+                  {barChartConfig[chart].label}
                 </span>
                 <span className="text-lg font-bold leading-none sm:text-3xl">
                   {total[key as keyof typeof total].toLocaleString()}
@@ -54,11 +73,11 @@ const TotalSalesChart: React.FC<TotalSalesChartProps> = ({ activeChart, setActiv
               </button>
             );
           })}
-        </div>
+        </div> */}
       </CardHeader>
       <CardContent className="px-2 sm:p-6">
         <ChartContainer
-          config={chartConfig}
+          config={barChartConfig}
           className="aspect-auto h-[250px] w-full"
         >
           <BarChart
@@ -80,7 +99,7 @@ const TotalSalesChart: React.FC<TotalSalesChartProps> = ({ activeChart, setActiv
                 const date = new Date(value);
                 return date.toLocaleDateString("en-US", {
                   month: "short",
-                  day: "numeric",
+                  year: "numeric",
                 });
               }}
             />
@@ -99,10 +118,7 @@ const TotalSalesChart: React.FC<TotalSalesChartProps> = ({ activeChart, setActiv
                 />
               }
             />
-            <Bar
-              dataKey={activeChart}
-              fill={`var(--color-${activeChart})`}
-            />
+            <Bar dataKey={activeChart} fill={`var(--color-${activeChart})`} />
           </BarChart>
         </ChartContainer>
       </CardContent>
